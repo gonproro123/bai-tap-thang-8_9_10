fa7f03cf977552c4334b6b66f23cc42797842fcd
Taengoo83 <43955691+Taengoo83@users.noreply.github.com>
2018-10-08 21:23:26 +0700

    FIBRP

--- main.cpp
+++ main.cpp
@@ -2,41 +2,53 @@
 
 using namespace std;
 
-const int N = 1e3 + 5;
+const int N = 100 + 5;
 
-int n, res, a[N], F[N];
+int s, k, F[N], res[N];
 
-void Input()
+void get_fibo()
 {
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> a[i];
-        F[i] = a[i];
-    }
+    F[0] = -1, F[1] = 1, F[2] = 2;
+    for (int i = 3; i <= N - 5; i++)
+        F[i] = F[i - 1] + F[i - 2];
 }
 
-void Solve()
+void Print(int n)
 {
-    for (int i = 2; i <= n; i++)
+    for (int i = 1; i < n; i++)
+        cout << res[i] << "+";
+    cout << res[n] << '\n';
+}
+
+void Solve(int i, int last, int c, int cnt, int sum)
+{
+    res[cnt] = F[i];
+    if (sum > s)
+        return;
+    if (sum == s)
     {
-        for (int j = 1; j < i; j++)
-        {
-            if (a[i] > a[j])
-                F[i] = max(F[i], F[j] + a[i]);
-        }
+        Print(cnt);
+        return;
     }
 
-    for (int i = 1; i <= n; i++)
-        res = max(res, F[i]);
-    cout << res << '\n';
+    if (F[i] == last && c + 1 <= k)
+        Solve(i, last, c + 1, cnt + 1, sum + F[i]);
+
+    for (int t = i + 1; t <= N - 5; t++)
+    {
+        if (sum + F[t] > s) return;
+        Solve(t, F[t], 1, cnt + 1, sum + F[t]);
+    }
 }
 
 int main()
 {
-    freopen("MAXIS.INP", "r", stdin);
-    freopen("MAXIS.OUT", "w", stdout);
-    Input();
-    Solve();
+    freopen("FIBRP.INP", "r", stdin);
+    freopen("FIBRP.OUT", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    get_fibo();
+    cin >> s >> k;
+    Solve(0, 0, 0, 0, 0);
     return 0;
 }
